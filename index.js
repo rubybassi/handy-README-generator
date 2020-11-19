87bd7c8c7731d4c05d27a698cc8223414d0b2b94
Sandeep Bassi <thecodingfiles@gmail.com>
2020-11-19 00:49:24 +0000

    added intial user response function, test questions and .md template

---
+++ index.js
@@ -0,0 +1,97 @@
+const fs = require("fs").promises;
+const inquirer = require("inquirer");
+
+const userResponse = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your project title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Enter a concise description about your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Give brief instructions on installation.",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Enter a brief description on the usage of the project.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message:
+        "State if you are open to contributions and what your requirements are for accepting them.",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message:
+        "Briefly explain any instructions for running tests.",
+      name: "tests",
+    },
+    {
+        type: "list",
+        message:
+          "Please select a license for your project from the list:",
+        choices: [],  
+        name: "license",
+      },
+      {
+        type: "input",
+        message:
+          "Please enter your github username (without spaces)",
+        name: "username",
+      },
+  ]);
+
+let buildReadme = (answers) =>
+  `![](https://img.shields.io/badge/license-${answers.license}-Green)
+
+# ${answers.title}
+
+### Description
+${answers.description}
+
+## Table of Contents
+- [Installation](Installation)
+- [Usage](Usage)
+- [Contributing](Contributing)
+- [Test](Test)
+- [License](License)
+- [Questions](Questions)
+
+### Installation
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+${answers.installation}
+
+### Usage
+${answers.usage}
+
+### Contributing
+${answers.contribution}
+
+### Test
+${answers.test}
+
+### License
+${answers.license}
+
+### Questions
+Please contact me on GitHub.
+
+[GitHub: ${answers.username}](https://github.com/${answers.username})
+`;
+
+userResponse()
+  .then((answers) => {
+    const md = buildReadme(answers);
+    fs.writeFileSync("output.md", md);
+  })
+  .then(() => console.log("successfully wrote to markdown"))
+  .catch(() => console.error(err));
