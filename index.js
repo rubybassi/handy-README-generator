a85a78ddecbae07d98392a5aac27c3ccd34359a4
Sandeep Bassi <thecodingfiles@gmail.com>
2020-11-20 22:57:57 +0000

    added email prompt, switched to heading 2s and edited license choices

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userResponse = () =>
     },
     {
       type: "input",
-      message: "Enter a concise description about your project.",
+      message: "Enter a concise description about your project. Use <br> to for a new line",
       name: "description",
     },
     {
@@ -37,7 +37,7 @@ const userResponse = () =>
     {
       type: "list",
       message: "Please select a license for your project from the list:",
-      choices: ["MIT", "ISC", "Creative Commons", "Apache License 2.0", "GNU GPLv2", "GNU GPLv3 f","No License"],
+      choices: ["MIT", "ISC", "CC", "Apache", "GNU","None"],
       default: "No License",
       name: "license",
     },
@@ -46,6 +46,11 @@ const userResponse = () =>
       message: "Please enter your github username (without spaces)",
       name: "username",
     },
+    {
+      type: "input",
+      message: "Please enter your email address",
+      name: "email",
+    },
   ]);
 
 const buildReadme = (answers) =>
@@ -53,7 +58,7 @@ const buildReadme = (answers) =>
 
 # ${answers.title}
 
-### Description
+## Description
 ${answers.description}
 
 ## Table of Contents
@@ -64,26 +69,26 @@ ${answers.description}
 - [License](License)
 - [Questions](Questions)
 
-### Installation
+## Installation
 Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
 ${answers.installation}
 
-### Usage
+## Usage
 ${answers.usage}
 
-### Contributing
+## Contributing
 ${answers.contribution}
 
-### Test
+## Test
 ${answers.test}
 
-### License
+## License
 ${answers.license}
 
-### Questions
-Please contact me on GitHub.
+## Questions
+Follow me on [GitHub: ${answers.username}](https://github.com/${answers.username})
 
-[GitHub: ${answers.username}](https://github.com/${answers.username})
+If you have any additional questions, simply email me at <${answers.email}> and i'll respond as soon as possible.  
 `;
 
 userResponse()
@@ -91,5 +96,5 @@ userResponse()
     const md = buildReadme(answers);
     fs.writeFileSync("output.md", md);
   })
-  .then(() => console.log("successfully wrote to markdown"))
+  .then(() => console.log("Great news! Your README markdown file has been succesfully created!"))
   .catch(() => console.error(err));
