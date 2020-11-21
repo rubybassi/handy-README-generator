2b7c4f0f9075636987f8483959d30cb31ab861ab
Sandeep Bassi <thecodingfiles@gmail.com>
2020-11-21 00:55:01 +0000

    added welcome message, edited default selection and removed test comment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const emoji = require('node-emoji')
+
+const welcomeMessage =
+  `Welcome to the Handy READme Generator! 
+  You will be asked a series of nine questions to help build your markdown file. 
+  To create a new line use <br>`;
+  
+console.log(emoji.get('pizza'), welcomeMessage);
 
 const userResponse = () =>
   inquirer.prompt([
@@ -10,45 +18,45 @@ const userResponse = () =>
     },
     {
       type: "input",
-      message: "Enter a concise description about your project. Use <br> to for a new line",
+      message: "Enter a concise description about your project:",
       name: "description",
     },
     {
       type: "input",
-      message: "Give brief instructions on installation.",
+      message: "Give brief instructions on installation:",
       name: "installation",
     },
     {
       type: "input",
-      message: "Enter a brief description on the usage of the project.",
+      message: "Enter a brief description on the usage of the project:",
       name: "usage",
     },
     {
       type: "input",
       message:
-        "State if you are open to contributions and what your requirements are for accepting them.",
+        "State if you are open to contributions and what your requirements are for accepting them:",
       name: "contribution",
     },
     {
       type: "input",
-      message: "Briefly explain any instructions for running tests.",
+      message: "Briefly explain any instructions for running tests;",
       name: "tests",
     },
     {
       type: "list",
       message: "Please select a license for your project from the list:",
-      choices: ["MIT", "ISC", "CC", "Apache", "GNU","None"],
-      default: "No License",
+      choices: ["MIT", "ISC", "CC", "Apache", "GNU", "None"],
+      default: "None",
       name: "license",
     },
     {
       type: "input",
-      message: "Please enter your github username (without spaces)",
+      message: "Please enter your github username:",
       name: "username",
     },
     {
       type: "input",
-      message: "Please enter your email address",
+      message: "Please enter your email address:",
       name: "email",
     },
   ]);
@@ -70,7 +78,6 @@ ${answers.description}
 - [Questions](Questions)
 
 ## Installation
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
 ${answers.installation}
 
 ## Usage
@@ -88,13 +95,12 @@ ${answers.license}
 ## Questions
 Follow me on [GitHub: ${answers.username}](https://github.com/${answers.username})
 
-If you have any additional questions, simply email me at <${answers.email}> and i'll respond as soon as possible.  
-`;
+If you have any additional questions, simply email me at <${answers.email}> and i'll respond as soon as possible.`;
 
 userResponse()
   .then((answers) => {
     const md = buildReadme(answers);
     fs.writeFileSync("output.md", md);
   })
-  .then(() => console.log("Great news! Your README markdown file has been succesfully created!"))
+  .then(() => console.log(emoji.get('white_check_mark'),"Great news! Your README markdown file has been succesfully created!"))
   .catch(() => console.error(err));
